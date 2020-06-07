bf1d84154b6235431e994cf4926ff7aa536dfa62
charlie-neitzen <51794636+Charlie-neitzen@users.noreply.github.com>
2020-06-07 17:08:17 +0100

    Added quit button

--- Onoschorch Mast/Assets/Scripts/SceneSwitch.cs
+++ Onoschorch Mast/Assets/Scripts/SceneSwitch.cs
@@ -5,11 +5,5 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitch : MonoBehaviour
 {
-    public void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.G))
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        }
-    }
+
 }
