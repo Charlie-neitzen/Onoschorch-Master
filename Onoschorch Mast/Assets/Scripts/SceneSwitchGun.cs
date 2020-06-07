3a5d355301d1a9b6c86b799ba1b1d29e6471bb01
charlie-neitzen <51794636+Charlie-neitzen@users.noreply.github.com>
2020-06-07 18:04:30 +0100

    Removed pause menu

--- Onoschorch Mast/Assets/Scripts/SceneSwitchGun.cs
+++ Onoschorch Mast/Assets/Scripts/SceneSwitchGun.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneSwitch : MonoBehaviour
+public class SceneSwitchGun : MonoBehaviour
 {
     public void Update()
     {
         if (Input.GetKeyDown(KeyCode.G))
         {
-            SceneManager.LoadScene("GunShop");
+            SceneManager.LoadScene("Main");
         }
     }
 }
